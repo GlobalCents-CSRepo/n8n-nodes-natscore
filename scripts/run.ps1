52925944c13cafd82bafd11d5688a00dcb2cd329
Pierre Nocera <pnocera@gmail.com>
2025-05-09 12:38:08 +0200

    deps fix

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -2,6 +2,18 @@
 # Equivalent to the systemd service configuration
 
 # Stop and remove existing container if it exists
+
+# copy dist folder content to data/custom folder
+# delete data/custom folder content first
+
+if (Test-Path -Path "${PWD}/data/custom") {
+		Remove-Item -Path "${PWD}/data/custom/*" -Recurse -Force
+} else {
+		New-Item -Path "${PWD}/data/custom" -ItemType Directory
+}
+
+Copy-Item -Path "${PWD}/dist/*" -Destination "${PWD}/data/custom" -Recurse -Force
+
 try {
     docker exec n8n stop
 } catch {
@@ -20,7 +32,6 @@ docker pull docker.n8n.io/n8nio/n8n
 # Run the container
 docker run --rm --name n8n `
     -v ${PWD}/data:/home/node/.n8n `
-    -v ${PWD}/dist:/home/node/.n8n/custom `
     -p 5678:5678 `
     -e N8N_EDITOR_BASE_URL='http://localhost:5678' `
     -e N8N_RUNNERS_ENABLED=true `
