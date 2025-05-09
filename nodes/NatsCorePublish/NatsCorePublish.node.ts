4ebd7bddf836b6fe89188c7b1921349ad6b49241
Pierre Nocera <pnocera@gmail.com>
2025-05-09 10:50:01 +0200

    linter fix

--- nodes/NatsCorePublish/NatsCorePublish.node.ts
+++ nodes/NatsCorePublish/NatsCorePublish.node.ts
@@ -8,7 +8,7 @@ import {
 import { Authenticator, ConnectionOptions, NatsConnection, nkeyAuthenticator } from '@nats-io/nats-core';
 import { connect } from "@nats-io/transport-node";
 import util from 'util';
-import { NatsCorePublishDescription } from './NatsCorePublish.description';
+
 
 export class NatsCorePublishNode implements INodeType {
 
@@ -107,4 +107,63 @@ export class NatsCorePublishNode implements INodeType {
 		}
 		return [items];
 	}
+}
+
+export const NatsCorePublishDescription: INodeTypeDescription = {
+	displayName: 'NATS Core Publish',
+	name: 'natsCorePublish',
+	icon: 'file:nats.svg',
+	group: ['transform'],
+	version: 1,
+	description: 'Sends messages to a NATS.io Server',
+	defaults: {
+		name: 'NATS Core Publish',
+	},
+	inputs: ['main'],
+	outputs: ['main'],
+	credentials: [
+		{
+			name: 'natsNkeyApi',
+			required: false,
+		},
+	],
+	properties: [
+		{
+			displayName: 'Subject',
+			name: 'subject',
+			type: 'string',
+			default: '',
+			required: true,
+			placeholder: 'subject-name',
+			description: 'Name of the queue of subject to publish to',
+		},
+		{
+			displayName: 'Message',
+			name: 'message',
+			type: 'string',
+			default: '',
+			description:
+				'The message to be sent. Can be JSON or a string. If JSON, set the "JSON Parameters" option.',
+		},
+		{
+			displayName: 'JSON Parameters',
+			name: 'jsonParameters',
+			type: 'boolean',
+			default: false,
+		},
+		{
+			displayName: 'Send Input Data',
+			name: 'sendInputData',
+			type: 'boolean',
+			default: true,
+			description: 'Whether to send the the data the node receives as JSON to NATS Core'
+		},
+		{
+			displayName: 'Queue Group Name',
+			name: 'queueGroupName',
+			type: 'string',
+			default: '',
+			description: 'The name of the queue group to use'
+		}
+	]
 }
